10abf4a23be8cff1cc597e23e4037e64e50004fb
bilel <hb.bilel@gmail.com>
2019-11-25 09:49:55 +0100

    unfinished

--- tree-map/tree-map.js
+++ tree-map/tree-map.js
@@ -33,5 +33,46 @@ var Tree = function(value) {
   this.children = [];
 };
 
+Tree.prototype.map = function (cb) {
+  var newTree = new Tree(cb(this.value))
+    for (var i = 0; i < this.children.length; i++) {
+      //var value = this.value;
+       //console.log(value)
+      //var newTree = new Tree(cb(this.value))
+      //console.log(newTree)
+     // cb(newTree.value);
+      //console.log()
+      //var obj = this.children[i]
+      newTree.children[i] = this.children[i].map(cb);
+      //return this.children[i].map(cb);
+    }
+   return newTree;
+}
+Tree.prototype.addChild = function(value) {
+  this.children.push(new Tree(value));
+  return new Tree(value);
+}
+
+var root1 = new Tree(1);
+
+var branch2 = root1.addChild(2);
+var branch3 = root1.addChild(3);
+    //
+var leaf4 = branch2.addChild(4);
+var leaf5 = branch2.addChild(5);
+
+var leaf6 = branch3.addChild(6);
+var leaf7 = branch3.addChild(7);
+
+var newTree = root1.map(function (value) {
+  return value * 2;
+})
+
+newTree.value // 2
+console.log(newTree.children[0].value) // 4
+console.log(newTree.children[1].value) // 6
 
 
+  newTree.children[0].children[1].value // 10
+  newTree.children[1].children[1].value // 14
+  root1.value // still 1
